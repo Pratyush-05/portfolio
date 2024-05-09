5ee7d4ce47a199d888e434567e96a3b7ce64956f
Pratyush-05 <purohit.pratyush05@gmail.com>
2024-05-10 04:02:52 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Contact from './components/Contact';
 
 import './App.css';
 function App() {
